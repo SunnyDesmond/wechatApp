62ec7eaaea4533a033b15074088f094f1b9bfac6
sunnyDesmond <sunny_desmond@sina.com>
2017-09-05 14:02:18 +0800

    new page

---
+++ client/pages/index/index.js
@@ -0,0 +1,105 @@
+// 引用百度地图微信小程序JSAPI模块 
+var bmap = require('../../libs/bmap-wx.js');
+var util = require('../../utils/util.js');  
+Page({
+  data: {
+    weatherModal:"小呆比"
+  },
+  onLoad: function () {
+    var that = this;
+    wx.showLoading({
+      title: '拼命加载中~',
+      success: function () {
+        // 获取用户信息
+        wx.getUserInfo({
+          success: function (res) {
+            let userInfo = res.userInfo
+            let nickName = userInfo.nickName
+            let avatarUrl = userInfo.avatarUrl
+            let gender = userInfo.gender //性别 0：未知、1：男、2：女
+            let province = userInfo.province
+            let city = userInfo.city
+            let country = userInfo.country
+            that.setData({
+              nickName,
+              avatarUrl,
+              gender,
+              province,
+              city,
+              country,
+            });
+            timeTips(); //时间问候
+          }
+        });
+        // 发起weather请求 
+        BMap.weather({
+          fail: fail,
+          success: success
+        });
+        
+      },
+      fail: function () {
+        wx.showToast({
+          title: '网络炸了！',
+          icon: 'success',
+          duration: 2000
+        });
+        wx.hideLoading();
+      }
+    });
+    // 新建百度地图对象 
+    var BMap = new bmap.BMapWX({
+      ak: 'vRDfB8MAzVWGofPPQVvrG9og2gqLGWFG'
+    });
+    var fail = function (data) {
+      console.log(data)
+    };
+    var success = function (data) {
+      var weatherData = data.currentWeather[0];
+      that.setData({
+        currentCity: weatherData.currentCity,
+        pm25: weatherData.pm25,
+        date: weatherData.date,
+        temperature: weatherData.temperature,
+        weatherDesc: weatherData.weatherDesc,
+        wind: weatherData.wind,
+        weatherModal: `${weatherData.weatherDesc}，${weatherData.temperature}`
+      });
+      wx.hideLoading();
+    }
+    // 时间段提示
+    function timeTips(){
+      let time = util.formatTime(new Date());  //获取本地日期 时间
+      let timeNow = time.split(" ")[1];   //获取本地时间 时：分：秒 
+      let hourNow = timeNow.split(":")[0]  //获取本地时间 时
+      if ( hourNow>=6 && hourNow<12){
+        wx.setNavigationBarTitle({
+          title: `早上好，${that.data.nickName}`
+        });
+      } else if (hourNow>=12 && hourNow<18){
+       
+        wx.setNavigationBarTitle({
+          title: `下午好，${that.data.nickName}`
+        });
+      } else if (hourNow>=18&&hourNow<24){
+        wx.setNavigationBarTitle({
+          title: `晚上好，${that.data.nickName}`
+        });
+      } else if(hourNow>=0&&hourNow<6){
+        wx.setNavigationBarTitle({
+          title: `深夜啦~，${that.data.nickName}`
+        });
+      }
+    }
+
+  },
+  weatherTapInfo:function(){
+    wx.showModal({
+      title: '今日天气',
+      content: this.data.weatherModal,
+      showCancel:false,
+      confirmText:'我知道了'
+    })
+  }
+
+});
